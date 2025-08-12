5845d421470b4d8469aa407c58c3f1075cfcfc86
MMohamed000 <moamenmohamedwork@gmail.com>
2025-08-12 09:33:57 +0300

    CURD DOM

--- NTI-MEAN STACK/Day6/main.js
+++ NTI-MEAN STACK/Day6/main.js
@@ -1,22 +1,24 @@
 let arrayList=[];
-let i=1;
 
-
-let create=(obj)=>{
+let create=()=>{
+    let Name=document.getElementById("name").value;
+    let disc=document.getElementById("desc").value;
+    let categ=document.getElementById("category").value;
+    let i=arrayList.length?arrayList.length:0;
+    let obj={name:Name, discription:disc,cat:categ,index:i+1};
     arrayList.push(obj);
     localStorage.setItem("LocalProducts",JSON.stringify(arrayList));
     table=document.getElementById("tbdy");
     table.innerHTML+= `
-        <tr id="${i}">
-        <td>${i}</td>
+        <tr id="${obj.index}">
+        <td>${obj.index}</td>
         <td>${obj.name}</td>
         <td>${obj.discription}</td>
         <td>${obj.cat}</td>
-        <td><input type="submit" value="Update"style="margin-right:30px;" onclick="update(${i});">
-        <input type="submit" value="Delete" onclick="del(${i})"></td>
+        <td><input type="submit" value="Update"style="margin-right:30px;" onclick="update(${obj.index});">
+        <input type="submit" value="Delete" onclick="del(${obj.index})"></td>
         </tr>
     `;
-    i+=1;
 }
 
 
@@ -36,7 +38,6 @@ let read=()=>{
             <input type="submit" value="Delete" onclick="del(${obj.index})"></td>
             </tr>
         `;
-        if(obj.index>=i)i=obj.index+1;
     }
 }
 
@@ -52,51 +53,35 @@ let update=(indx)=>{
         let Name=document.getElementById("name").value;
         let disc=document.getElementById("desc").value;
         let categ=document.getElementById("category").value;
+
+        obj.name=Name!==""?Name:obj.name;
+        obj.discription=disc!==""?disc:obj.discription;
+        obj.cat=categ!==""?categ:obj.cat;
+
         tr.innerHTML=`
-        <td>${indx}</td>
-        <td>${Name}</td>
-        <td>${disc}</td>
-        <td>${categ}</td>
-        <td><input type="submit" value="Update"style="margin-right: 45px;" onclick="update(${obj.index});">
+        <td>${obj.index}</td>
+        <td>${obj.name}</td>
+        <td>${obj.discription}</td>
+        <td>${obj.cat}</td>
+        <td><input type="submit" value="Update"style="margin-right: 30px;" onclick="update(${obj.index});">
         <input type="submit" value="Delete" onclick="del(${obj.index});"></td>
     `
-        obj.name=Name;
-        obj.discription=disc;
-        obj.cat=categ;
+    localStorage.setItem("LocalProducts",JSON.stringify(arrayList));
         let intID=setInterval(()=>{
             element.value="Add";
             element.removeEventListener("click", Update);
             clearInterval(intID);
         },2000);
     });
-
 }
 
 
 let del=(index)=>{
     let tbody=document.getElementById("tbdy");
     tbody.innerHTML='';
-    let bool=false;
+    arrayList.splice(index,1);
     for(let j=0; j<arrayList.length;++j){
         let obj=arrayList[j];
-        if(bool){
-            tbody.innerHTML+=`
-            <tr id="${j}">
-            <td>${j}</td>
-            <td>${obj.name}</td>
-            <td>${obj.discription}</td>
-            <td>${obj.cat}</td>
-            <td><input type="submit" value="Update"style="margin-right:30px;" onclick="update(${j});">
-            <input type="submit" value="Delete" onclick="del(${j})"></td>
-            </tr>
-        `;
-            continue;
-        }
-        if(j===(index-1) && !bool){
-            bool=true;
-            arrayList.splice(j,1);
-            continue;
-        }
         tbody.innerHTML+=`
         <tr id="${j+1}">
         <td>${j+1}</td>
@@ -107,26 +92,44 @@ let del=(index)=>{
         <input type="submit" value="Delete" onclick="del(${j+1})"></td>
         </tr>
         `;
+        obj.index=j+1;
     }
-    i=arrayList.length;localStorage.setItem("LocalProducts",JSON.stringify(arrayList));
+    i=arrayList.length+1;
+    localStorage.setItem("LocalProducts",JSON.stringify(arrayList));
 }
 
 
 let search=()=>{
     let type=document.getElementById("searchForm");
     type.addEventListener("submit",function doWork(event){
-        let category=document.getElementById("searchCategory").value;
+        let category=document.getElementById("searchCategory").value.trim();
         let Name='';
-        Name=document.getElementById("searchInput").value;
+        Name=document.getElementById("searchInput").value.trim();
         event.preventDefault();
         let tbody=document.getElementById("tbdy");
         tbody.innerHTML='';
         if(category.toLowerCase()==="all"){
-            read();
+            if(Name==="")read();
+            else{
+                 for(let obj of arrayList){
+                    if((obj.name.toLowerCase()===Name.toLowerCase())){
+                        tbody.innerHTML+=`
+                        <tr id="${obj.index}">
+                        <td>${obj.index}</td>
+                        <td>${obj.name}</td>
+                        <td>${obj.discription}</td>
+                        <td>${obj.cat}</td>
+                        <td><input type="submit" value="Update"style="margin-right:30px;" onclick="update(${obj.index});">
+                        <input type="submit" value="Delete" onclick="del(${obj.index})"></td>
+                        </tr>
+                        `;
+                    }
+                }
+            }
             return;
         }
         for(let obj of arrayList){
-            if(obj.cat.toLowerCase()===category.toLowerCase() && (obj.name.toLowerCase()===Name.toLowerCase() || Name==='')){
+            if((obj.cat.toLowerCase()===category.toLowerCase()) && (obj.name.toLowerCase()===Name.toLowerCase() || Name==='')){
                 tbody.innerHTML+=`
                 <tr id="${obj.index}">
                 <td>${obj.index}</td>
@@ -147,11 +150,7 @@ let search=()=>{
 let form = document.getElementById("myform");
 form.addEventListener("submit", function(event) {
 event.preventDefault();
-let Name=document.getElementById("name").value;
-let disc=document.getElementById("desc").value;
-let categ=document.getElementById("category").value;
-let obj={name:Name, discription:disc,cat:categ,index:i}
-    create(obj);
+    create();
 });
 
 
