daf6e82b412f3c1ac807c992b144d37c7d4ca12b
MMohamed000 <moamenmohamedwork@gmail.com>
2025-08-21 17:59:08 +0300

    Delete NTI-MEAN STACK/Day11 directory

--- NTI-MEAN STACK/Day11/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
-import { routes } from './app.routes';
-
-
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes,withHashLocation() , withInMemoryScrolling({scrollPositionRestoration:'top'}))]
-};
